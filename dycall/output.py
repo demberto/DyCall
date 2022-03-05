1028d8802fbd9bdbb9b78230ae9e934410480b9c
demberto <shahanand64@gmail.com>
2022-03-06 00:26:25 +0530

    Added logging

--- dycall/output.py
+++ dycall/output.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import logging
 
 import ttkbootstrap as tk
@@ -11,6 +12,7 @@ log = logging.getLogger(__name__)
 
 class OutputFrame(ttk.Labelframe):
     def __init__(self, _: tk.Window, output: tk.StringVar):
+        log.debug("Initialising")
         super().__init__(text=MC.translate("Output"))
         self.oe = oe = ttk.Entry(
             self,
@@ -21,3 +23,4 @@ class OutputFrame(ttk.Labelframe):
         self.oc = oc = CopyButton(self, output, state="disabled")
         oc.pack(side="right", padx=(0, 5), pady=5)
         oe.pack(fill="x", padx=5, pady=5)
+        log.debug("Initialised")
